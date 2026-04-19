cb5be7c92b988bbed9cb6795065631d6faa47fc9
saraashfaq3 <saraashfaq498@gmail.com>
2026-04-19 00:07:17 -0800

    Operation

--- 03_ConversionorOperation.js
+++ 03_ConversionorOperation.js
@@ -39,3 +39,16 @@ console.log(3 + 4 * 5 % 3); //wrong way   //5
 console.log(+true);  //1
 console.log(+"");   //0
 // console.log(true+);  //error
+
+
+//Postfix Increment x++ 
+let x = 3;
+const y = x++;
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+//Prefix Increment  ++x
+let a = 3;
+const b = ++a;
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
