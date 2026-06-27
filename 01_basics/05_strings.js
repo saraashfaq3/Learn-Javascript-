88d82b73e466587b20f4079f2bdc64cedf447bcf
saraashfaq3 <saraashfaq498@gmail.com>
2026-06-26 22:20:32 -0800

    strings

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -5,40 +5,40 @@
 //In program string is written in variables->
 const name = "sara";
 const repoCount = 50;
-console.log(name + repoCount + "value"); //This syntax is outdated don't use it.
+console.log(name + repoCount + "value"); //This syntax is outdated don't use it. //sara50value
 
 //Use this string Interpolation-> is k andar methods bhi sath hi perform k sakty hain
 // e.g; ${name.ToUperCase}
-console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);  //Hello my name is sara and my repo count is 50
 
 //String ko declared karny k aik or tarika bhi ha .string aik object ha.
 const gameName = new String("sara-ashfaq-3"); //new aik keyword ha(objects use karty hain javascript k),
 // ('')->constructor
 //Answer:[String: 'sara'],positioning (0:s,1:a,2:r,3:a), prototype(methods)
-console.log(gameName); //[String: 'sara']
+console.log(gameName); //[String: 'sara-ashfaq-3']
 //Methods
-console.log(gameName[0]); //Keys access karny k tarika
-console.log(gameName.__proto__);   //prototype access karny k tarika
+console.log(gameName[0]); //Keys access karny k tarika  //s
+console.log(gameName.__proto__);   //prototype access karny k tarika  //{}
 
 //length
-console.log(gameName.length);
+console.log(gameName.length); //13
 
 //toUpperCase() toLowerCase()
-console.log(gameName.toUpperCase());
+console.log(gameName.toUpperCase()); //SARA-ASHFAQ-3
 
 //charAt
-console.log(gameName.charAt(3));  //kis index/position per konsa character ha 
+console.log(gameName.charAt(3));  //kis index/position per konsa character ha //a
 
 //indexOf
-console.log(gameName.indexOf("r"));  //character kis position/index per ha 
+console.log(gameName.indexOf("r"));  //character kis position/index per ha //2
 
 //substring
-const newString=gameName.substring(0,5);
-console.log(newString);
+const newString=gameName.substring(0,5); //0 se le kar 4 tak ki values aye gi 5 wali include nahi hogi
+console.log(newString);  //sara-
 
 //slice
-const anotherSring=gameName.slice(-9,4)
-console.log(anotherSring);
+const anotherSring=gameName.slice(-12,4)
+console.log(anotherSring); //ara
 
 // Trim remove starting space and end space
 const newStringOne="     sara     "
@@ -52,15 +52,16 @@ console.log(url.replace('%20','-'));
 //includes (kiya url mein sara likha how aha ya nahi)
 console.log(url.includes('sara'));
 
-//split()
-console.log(gameName.split('-'));
+//split() method ka use string ko multiple parts mein todne ke liye hota hai. 
+// Yeh array return karta hai.
+console.log(gameName.split('-')); //[ 'sara', 'ashfaq', '3' ]
 
 //concat,blink,bold,constructor,endswith,fix
 
 //concat (Strings ko join karta hai.)
 let str1="hello";
 let str2="world";
-console.log(str1.concat(" ",str2," ","sara"));
+console.log(str1.concat(" ",str2," ","sara"));  //hello world sara
 
 //blink (Yeh purana HTML method hai (ab use nahi hota).) don't use
 //bold(Text ko HTML bold bana deta hai.) don't use
